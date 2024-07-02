7d56ff4f3c63997d2f769a0f23f2a1d81189e07b
Matheus Baptistella <mbapts@gmail.com>
2024-07-01 22:09:17 -0300

    Finish the exercise

--- quiz/p3.c
+++ quiz/p3.c
@@ -12,7 +12,7 @@
 #include <stdio.h>
 
 int foo();
-int bar();
+char bar();
 
 int main()
 {
@@ -27,7 +27,7 @@ int foo(int n)
   return a;
 }
 
-int bar(int m)
+char bar(int m)
 {
   int b;
   b = m+1;
